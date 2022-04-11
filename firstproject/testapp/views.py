4be34844674848fc36ff29a65ad36b3d3ac60823
lagidiraju <lagidi.raju@gmail.com>
2022-04-11 14:35:36 -0700

    second commit

--- firstproject/testapp/views.py
+++ firstproject/testapp/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def display(request):
-    s='<h1>hello world</h1>'
+    s='<h1>hello world how are you?</h1>'
     return HttpResponse(s)
